b6c07c045ff01cbf7f9c1e0577057eb6ffe95586
Pedro Estrada <pedroestrada.86@gmail.com>
2025-11-23 10:20:24 -0500

    more test scripts

---
+++ samples/alert-test-script-1.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Alert Test Script (document-start)
+// @namespace    https://github.com/Morl0ck
+// @version      1.1
+// @description  Automatically skips the intro on Crunchyroll videos (enabled by default).
+// @author       Morl0ck
+// @match        https://*.github.com/*
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+alert("Hello");
---
+++ samples/alert-test-script-2.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Alert Test Script (document-idle)
+// @namespace    https://github.com/Morl0ck
+// @version      1.1
+// @description  Automatically skips the intro on Crunchyroll videos (enabled by default).
+// @author       Morl0ck
+// @match        https://*.github.com/*
+// @run-at       document-idle
+// @grant        none
+// ==/UserScript==
+
+alert("World");
